33a1818c1f5989fd4f5e4c9ca96d4c3f63e390c3
Kudryashova_Nastya <avk2001avk@gmail.com>
2023-03-13 05:36:18 +0300

    menu is done

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import './style.css';
 import {Link, Outlet} from "react-router-dom";
 import TUCOIN_MENU_MINI from "../../images/icons/Tucoin_menu_mini.svg"
+import NOTIFICATION from "../../images/icons/Notification.svg"
+import TUCOIN from "../../images/icons/Tucoin_menu.svg"
+import MARKET from "../../images/icons/Market.svg"
+import QUESTS from "../../images/icons/Quests.svg"
+import Achievements from "../../images/icons/Achievements.svg"
+import Curators from "../../images/icons/Curators.svg"
+import Students from "../../images/icons/Students.svg"
+import Traectoria from "../../images/icons/Traectoria.svg"
+import Events from "../../images/icons/Events.svg"
+import TELEGRAM from "../../images/icons/Telegram.svg"
+import VK from "../../images/icons/VK.svg"
+import SETTINGS from "../../images/icons/Settings.svg"
+import LOGO from "../../images/icons/Logo_white.svg"
+
 
 const Menu = () => {
     return (
@@ -10,8 +24,8 @@ const Menu = () => {
                 <div className="menu__info">
                     <div>
                         <div className="name">Иван Иванов</div>
-                        <div className="value"> 125&nbsp;
-                            <img src={TUCOIN_MENU_MINI} className="tucoin" width="17" height="17" alt="игровой валюты"/>
+                        <div className="value"> 125
+                            <img src={TUCOIN_MENU_MINI} className="tucoin" alt="игровой валюты"/>
                         </div>
                     </div>
                     <div>
@@ -20,10 +34,71 @@ const Menu = () => {
                     </div>
                 </div>
                 <ul className="menu__links">
-                    <li><Link to='/one'>Page One</Link></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={NOTIFICATION} className="ico" alt="уведомления"/>
+                            Уведомления
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={TUCOIN} className="ico" alt="TUCOIN"/>
+                            TUCOIN
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={MARKET} className="ico" alt="MARKET"/>
+                            Маркет
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={QUESTS} className="ico" alt="QUESTS"/>
+                            Витрина заданий
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={Achievements} className="ico" alt="Achievements"/>
+                            Зал ачивок
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={Curators} className="ico" alt="Curators"/>
+                            Кураторы и коучи
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={Students} className="ico" alt="Students"/>
+                            Студенты
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={Traectoria} className="ico" alt="Traectoria"/>
+                            Траектория
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/one'>
+                            <img src={Events} className="ico" alt="Events"/>
+                            Календарь мероприятий
+                        </Link>
+                    </li>
                 </ul>
+                <div className="social">
+                    <div>
+                        <Link to='/student'><img src={TELEGRAM} className="networks" alt="TELEGRAM"/></Link><br/>
+                        <Link to='/student'><img src={VK} className="networks" alt="VK"/></Link><br/>
+                        <Link to='/student'><img src={SETTINGS} className="networks" alt="SETTINGS"/></Link><br/>
+                    </div>
+                    <div>
+                        <Link to='/student'><img src={LOGO} className="networks logo" alt="LOGO"/></Link><br/>
+                    </div>
+                </div>
             </nav>
             <Outlet/>
         </>
