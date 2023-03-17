58f5f3205e0a716c4f08a60149781441c6dfc1b0
Kudryashova_Nastya <avk2001avk@gmail.com>
2023-03-17 14:34:10 +0300

    login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,36 +1,76 @@
 import React, {useState} from "react";
+import '../base.css';
+import './style.css'
+import Auth from "../../store/helper/Auth";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-	const [login, setLogin] = useState("");
+	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const history = useNavigate()
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+		const data = {
+			"email": email,
+			"password": password
+		}
+		const log = await Auth.login(data)
+		if (log) {
+			// setAuthErrors(await Auth.login(data))
+			console.log(log)
+		} else {
+			const role = Auth.getRole()
+			switch (role) {
+				case "student":
+					history("/student")
+					break;
+				case "manager":
+					history("/manager")
+					break;
+				case "main-admin":
+					history("/main-admin")
+					break;
+				case "curator":
+					history("/curator")
+					break;
+				case "couch":
+					history("/couch")
+					break;
+				default:
+					alert("Ты кто?")
+			}
+		}
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="login">Login:</label>
-					<input
-						name="login"
-						type="text"
-						value={login}
-						onChange={(e) => setLogin(e.target.value)}
-					/>
-				</div>
-				<div>
-					<label htmlFor="password">Password:</label>
-					<input
-						name="password"
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</div>
-				<button>Submit</button>
-			</form>
+		<div className="window-container">
+			<div className="window">
+				<h1 className="header3">Авторизация</h1>
+				<form className="form" onSubmit={handleSubmit}>
+					<div>
+						<label htmlFor="login">&nbsp;&nbsp;&nbsp;&nbsp;Email</label>
+						<input
+							className="input"
+							name="email"
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Пароль</label>
+						<input
+							className="input"
+							name="password"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<button className="button">Войти</button>
+				</form>
+			</div>
 		</div>
 	);
 };
