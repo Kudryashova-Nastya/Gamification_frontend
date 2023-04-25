fcf926e690ca2d85a60bb2c4beb20a9d744be7b7
Kudryashova_Nastya <avk2001avk@gmail.com>
2023-04-26 02:37:11 +0300

    transactions in bank page

--- src/components/Bank/Bank.jsx
+++ src/components/Bank/Bank.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import BIGTUCOIN from "../../images/icons/black-tucoin45.svg";
 import "./style.css"
 import '../base.css';
-import {CORS, getHostInformation} from "../../store/helper/Helper";
+import {CORS, getHostInformation, POSTCORS} from "../../store/helper/Helper";
 import 'react-loading-skeleton/dist/skeleton.css'
 import Auth from "../../store/helper/Auth";
 import Pagination from "../Pagination/Pagination";
@@ -14,29 +14,36 @@ const Bank = () => {
 	const history = useNavigate()
 
 	const [transactions, setTransactions] = useState([
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
+		{}, {}, {}, {}, {}
 	])
+	const [allTucoins, setAllTucoins] = useState("???")
 
 	// пагинация
 	const [currentPage, setCurrentPage] = useState(1)
 	const postsPerPage = 5
 
-
 	useEffect(() => {
 		const host = getHostInformation()
 		const fetchData = async () => {
 			const token = await Auth.getToken()
-			await fetch(`${host}/api/v1/short_student`, CORS(token?.access))
+			await fetch(`${host}/api/v1/bank/all_money_in_app`, CORS(token?.access))
 			.then((res) => res.json())
 			.then((data) => {
 				if (data.code === "token_not_valid") {
-					console.log("проблема протухшего токена в студентах, перезапуск запроса")
-					fetchData()
+					return fetchData()
+				} else {
+					setAllTucoins(data)
+				}
+			})
+			.catch((err) => {
+				console.log("err", err)
+			})
+
+			await fetch(`${host}/api/v1/transaction/all_transfers/`, POSTCORS({"transfer_type": "transfer"}, token?.access))
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.code === "token_not_valid") {
+					return fetchData()
 				} else {
 					setTransactions(data)
 				}
@@ -44,12 +51,7 @@ const Bank = () => {
 			.catch((err) => {
 				console.log("err", err)
 				const data = [
-					{},
-					{},
-					{},
-					{},
-					{},
-					{},
+					{}, {}, {}, {}, {}
 				]
 				setTransactions(data)
 			})
@@ -73,7 +75,7 @@ const Bank = () => {
 			<hr color="#CCCCCC" size="4"/>
 			<div className="bank-value-container">
 				<div className="bank-value">
-					<span className="balance-icon">5 232</span> <img className="balance-icon" src={BIGTUCOIN} alt="tucoin"/>
+					<span className="balance-icon">{allTucoins}</span> <img className="balance-icon" src={BIGTUCOIN} alt="tucoin"/>
 					<span className="bank-sub">в системе</span>
 				</div>
 			</div>
--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -3,23 +3,45 @@ import '../base.css';
 import './Transaction.css'
 import TUCOIN from '../../images/icons/black-tucoin16.svg'
 import ARROWTO from '../../images/icons/arrow-to.svg'
+import Skeleton from "react-loading-skeleton";
 
 const Transaction = ({el}) => {
+	let options = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		timezone: 'UTC',
+		hour: 'numeric',
+		minute: 'numeric'
+	}
+	let datetime = new Date(el.date_time)
+
+
+	// const date = new Date();
+	// const minutes = date.getMinutes().toString().padStart(2, '0');
+	// const seconds = date.getSeconds().toString().padStart(2, '0');
+	// const day = date.getDate().toString().padStart(2, '0');
+	// const monthIndex = date.getMonth();
+	// const month = new Intl.DateTimeFormat('ru', { month: 'short' }).format(date);
+	// const year = date.getFullYear();
+	// const formattedDate = `${minutes}:${seconds} ${day} ${month} ${year}`;
+	// console.log(formattedDate); // "мм:сс дд месяц гггг"
 
 	return (
 		<div className="transaction-block">
-			<div className="datetime">20 декабря 12:20</div>
+			<div className="datetime">{datetime.toLocaleString("ru-RU", options) || ""}</div>
 			<div className="transaction">
 				<div className="groups">
-					<div className="member sender">Валерий Жмышенко</div>
+					<div className="member sender">{el.from_id?.first_name || <Skeleton width={105}/>} {el.from_id?.last_name}</div>
 					<div className="ico"><img alt="arrow" src={ARROWTO}/></div>
-					<div className="member">Петя Пимашков</div>
+					<div className="member">{el.to_id?.first_name || <Skeleton width={105}/>} {el.to_id?.last_name}</div>
 				</div>
 				<div className="groups info">
 					<div className="count">
-						<span className="balance-icon">+20</span> <img className="balance-icon" alt="tucoin" src={TUCOIN}/>
+						<span className="balance-icon">{el.sum_count || <Skeleton width={25}/>} </span>
+						{el.sum_count && <img className="balance-icon" alt="tucoin" src={TUCOIN}/>}
 					</div>
-					<div className="message">Моему братишке братюличке любимому на прокачку)</div>
+					<div className="message">{el.comment || <Skeleton width={90}/>}</div>
 				</div>
 			</div>
 		</div>
