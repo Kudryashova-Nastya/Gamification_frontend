9234f49e662a1d2638470b2d2a1077102ddbface
Kudryashova_Nastya <avk2001avk@gmail.com>
2023-05-20 04:12:04 +0300

    quests tags

--- src/components/Quests/Quests.jsx
+++ src/components/Quests/Quests.jsx
@@ -32,28 +32,21 @@ const Quests = () => {
 	const postsPerPage = 10
 
 	// сортировка
-	const [sortBy, setSortBy] = useState('id');
+	const [sortBy, setSortBy] = useState('all');
 
-	const handleSortChange = (event) => {
+	const handleFilterChange = (event) => {
 		setSortBy(event.target.value);
-		const sorted = sortQuests(event.target.value, arr)
+		const sorted = filterQuests(event.target.value, quests)
 		setArr(sorted)
 	};
 
-	const sortQuests = (by, arr) => {
-		// if (arr[0]?.last_name) {
-		// 	return arr.sort((a, b) => {
-		// 		if (by === 'lastname') {
-		// 			return a.last_name.localeCompare(b.last_name)
-		// 		} else if (by === 'balance') {
-		// 			return b.balance - a.balance
-		// 		} else {
-		// 			return a.id - b.id || 0
-		// 		}
-		// 	})
-		// } else {
+	const filterQuests = (by, arr) => {
+		if (by === "all") {
 			return arr
-		// }
+		} else {
+			console.log(by)
+		return arr.filter((el)=>el.type == by)
+		}
 	}
 
 	useEffect(() => {
@@ -80,7 +73,7 @@ const Quests = () => {
 				<h1 className="header1">Витрина заданий</h1>
 				{!isLoading &&
 					<div className="two-elements-right">
-						<Search students={quests} setArr={setArr} setCurrentPage={setCurrentPage} sort={sortQuests}
+						<Search students={quests} setArr={setArr} setCurrentPage={setCurrentPage} sort={filterQuests}
 										by={sortBy}/>
 						<button onClick={() => history("my-quests")} className="button">
 							Мои задания <img src={MYQUESTS} className="button-icon" alt=""/>
@@ -90,26 +83,32 @@ const Quests = () => {
 			<hr color="#CCCCCC" size="4"/>
 			<div className="filter-container">
 				<div>Теги:</div>
-				<div className="filter-value">
-					<input type="radio" id="by-id" name="filter" value="id" checked={sortBy === 'id'}
-								 onChange={handleSortChange}/>
-					<label htmlFor="by-id">По умолчанию</label>
+				<div className="filter-value tag"
+						 style={{"background": '#CCCCCC40',
+							 "border": sortBy === 'all' ? '2px solid #CCCCCC' : 'none',
+							 "padding": sortBy === 'all' ? '5px 15px' : '7px 17px'
+						 }}>
+					<input type="radio" id="by-id" name="tags" value="all" checked={sortBy === 'all'}
+								 onChange={handleFilterChange}/>
+					<label htmlFor="by-id">Все</label>
 				</div>
-				<div className="filter-value">
-					<input type="radio" id="by-lastname" name="filter" value="lastname" checked={sortBy === 'lastname'}
-								 onChange={handleSortChange}/>
-					<label htmlFor="by-lastname">По фамилии</label>
-
-				</div>
-				<div className="filter-value">
-					<input type="radio" id="by-balance" name="filter" value="balance" checked={sortBy === 'balance'}
-								 onChange={handleSortChange}/>
-					<label htmlFor="by-balance">По балансу</label>
-				</div>
+				{QuestStore.questTypes.map((tag, i) =>
+					<div className="filter-value tag" key={i}
+							 style={{
+								 "background": `${tag?.color || '#CCCCCC'}40`,
+								 "border": sortBy == tag.id ? `2px solid ${tag?.color || '#CCCCCC'}70` : 'none',
+								 "padding": sortBy == tag.id ? '5px 15px' : '7px 17px'
+							 }}>
+						<input type="radio" id={tag.id} name="tags" value={tag.id} checked={sortBy === tag.id}
+									 onChange={handleFilterChange}/>
+						<label htmlFor={tag.id}>{tag.name}</label>
+					</div>
+				)}
 			</div>
 			<div className="student-container">
 				{currentPosts?.map((el, i) =>
-					<div key={i} className="quest-card" style={{"background": `${QuestStore.questTypes.find(color => color.id === el.type)?.color || '#CCCCCC'}40`}}>
+					<div key={i} className="quest-card"
+							 style={{"background": `${QuestStore.questTypes.find(color => color.id === el.type)?.color || '#CCCCCC'}40`}}>
 						<div className="title">
 							{el.name || <Skeleton width={100}/>}
 						</div>
@@ -117,14 +116,14 @@ const Quests = () => {
 							<div>
 								<div className="sum">
 									{el.sum ? <>
-									<span className="balance-icon">{el.sum} </span>
-									<img src={TUCOIN} alt="" className="balance-icon"/></>
-									: ""}
+											<span className="balance-icon">{el.sum} </span>
+											<img src={TUCOIN} alt="" className="balance-icon"/></>
+										: ""}
 								</div>
 								<button className="button btn-mini">Подробнее</button>
 							</div>
 							<div className="description">
-								{ el.description ? cutText(el.description) : <Skeleton width={150} count={2.5}/>}
+								{el.description ? cutText(el.description) : <Skeleton width={150} count={2.5}/>}
 							</div>
 						</div>
 					</div>
